0477fcca2bbb28a98b4fd15227b920f3f00f9f53
shijting <1269879928@qq.com>
2025-03-05 23:25:58 +0800

    向量搜索入门

--- vec_learn/README.md
+++ vec_learn/README.md
@@ -17,4 +17,63 @@ pip install faiss-cpu
 pip install faiss-gpu(可以你的电脑支持)
 ```
 
+# 向量数据库
+## faiss
+
+支持CPU和GPU计算可以处理海量的向量数据，支持多种索引方式提供了Python接口，可以与Numpy完美衔接。
+FAISS的优点是速度快、灵活、可扩展，缺点是安装依赖复杂、使用门槛高、不支持元数据存储。
+
+## Chroma
+
+本地数据库
+
+chroma是一个用于构建带有嵌入向量的AI应用程序的数据库，内置了入门所需的一切，并提供了简单的API。
+它目前只支持CPU计算，但可以利用乘积量化的方法将一个向量的维度切成多段，每段分别进行k-means，从而减少存储空间和提高检索效率。
+它还可以与Langchain集成，实现基于语言模型的应用。chroma的优点是易用、轻量、智能，缺点是功能相对简单、不支持GPU加速
+
+FAISS支持L2距离和内积两种相似度度量，chroma支持L2距离、内积和余弦相似度
+三种相似度度量。
+
+### 地址
+这是github地址
+https://github.com/chroma-core/chroma
+
+### 安装
+```bash
+pip install chromadb
+```
+
+## qdrant(推荐)
+官方介绍，adrant的优点包括
+1. 其高性能和高可扩展性，易于使用的API2、可以内存、硬盘、Docker、cloud等多种模式使用
+2. 有效管理计算资源，特别适用云原生部署
+3. rust语言编写，性能优秀，支持多种索引方式，支持多种相似度度量
+4. 支持多模态数据库/rag（+Aleph Alpha embedding技术）
+
+### 地址
+[官方地址](github.com/qdrant/gdrant-client)
+### 安装
+
+#### 服务端
+```bash
+docker pull qdrant/qdrant:v1.10.0
+
+docker run -d --name dqrant -p 6333:6333 \
+-v $(pwd)/dqrant_data:/dqrant/storage qdrant/qdrant:v1.10.0
+```
+在浏览器中输入http://localhost:6333 或者公网地址可以看到qdrant的管理界面
+
+如果要添加安全认证，可以使用nginx反向代理，进行安全认证（自行ai搜索）
+
+#### 客户端
+```bash
+pip install qdrant-client
+pip install -U langchain-qdrant
+```
+
+
+
+
+
+
 
--- vec_learn/vec.py
+++ vec_learn/vec.py
@@ -1,16 +1,95 @@
 import os
+from _md5 import md5
 
 from langchain_community.embeddings import DashScopeEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS, Chroma
+# 加载文本，还有pdf，word等等
+from langchain_community.document_loaders import TextLoader
+# 切割文本
+from langchain.text_splitter import CharacterTextSplitter
 
-def test():
+# qdrant
+from qdrant_client import QdrantClient, models
+from qdrant_client.models import Distance, VectorParams
+from langchain_qdrant import QdrantVectorStore
+
+
+def faiss_test():
     api_key = os.environ.get("TONGYI_API_KEY")
     eb = DashScopeEmbeddings(dashscope_api_key=api_key, model="text-embedding-v1")
     # print(eb.embed_query("今天天气怎么样"))
+
     # 数组里的内容可以理解为我们放在数据库里的内容，或者理解为我们的知识库
-    f = FAISS.from_texts(["今天天气是晴天"], eb)
+    # f = FAISS.from_texts(["今天天气是晴天", "明天星期三"], eb)
     # 我们的提问： 今天天气怎么样，得到欧氏距离越小越相似
-    print(f.similarity_search_with_score("今天天气怎么样"))
+    # print(f.similarity_search_with_score("今天天气怎么样"))
+
+    # 从文本中加载数据
+    text_loader = TextLoader(file_path="./resource/golang.txt").load()
+    # chunk_overlap 重叠的部分, 最好大于0, 实际开发中需要我们自己去调，否则搜索出来的结果不是很准。
+    # chunk_size 切割的大小
+    doc = CharacterTextSplitter(chunk_size=200, chunk_overlap=0).split_documents(text_loader)
+    # print(cts)
+    f = FAISS.from_documents(doc, eb)
+    print(f.similarity_search_with_score("程序员工资如何"))
+
+
+def chroma_store():
+    api_key = os.environ.get("TONGYI_API_KEY")
+    eb = DashScopeEmbeddings(dashscope_api_key=api_key, model="text-embedding-v1")
+    # 从文本中加载数据
+    text_loader = TextLoader(file_path="./resource/golang.txt").load()
+    # chunk_overlap 重叠的部分, 最好大于0, 实际开发中需要我们自己去调，否则搜索出来的结果不是很准。
+    # chunk_size 切割的大小
+    doc = CharacterTextSplitter(chunk_size=200, chunk_overlap=0).split_documents(text_loader)
+    # print(cts)
+    # 将分割后的文本存储到chroma中, 目前是存储到本地文件，当然也可以存储到手工安装的chroma 数据库中或者在其他云厂商托管的chroma数据库中
+    db = (Chroma(collection_name="golang", embedding_function=eb, persist_directory="./datadir/chroma")
+          .from_documents(doc, eb))
+    # 返回一堆id
+    ids = db.add_documents(doc)
+    print(ids)
+
+
+def chroma_search():
+    api_key = os.environ.get("TONGYI_API_KEY")
+    eb = DashScopeEmbeddings(dashscope_api_key=api_key, model="text-embedding-v1")
+    db = Chroma(collection_name="golang", embedding_function=eb, persist_directory="./datadir/chroma")
+    res = db.similarity_search_with_score("golang 程序员工资如何")
+    print(res)
+
+
+def qdrant_store():
+    api_key = os.environ.get("TONGYI_API_KEY")
+    eb = DashScopeEmbeddings(dashscope_api_key=api_key, model="text-embedding-v1")
+    # 从文本中加载数据
+    text_loader = TextLoader(file_path="./resource/golang.txt").load()
+    # chunk_overlap 重叠的部分, 最好大于0, 实际开发中需要我们自己去调，否则搜索出来的结果不是很准。
+    # chunk_size 切割的大小
+    docs = CharacterTextSplitter(chunk_size=200, chunk_overlap=0).split_documents(text_loader)
+    client = QdrantClient(host="localhost", port=6333)
+    # 创建一个collection, 也可以理解为一个数据库表
+    collection_name = "golang"
+    if not client.collection_exists(collection_name):
+        res = client.create_collection(collection_name=collection_name,
+                                       vectors_config=VectorParams(size=eb.embedding_dimensions,  # 向量的维度,通义千问事1536
+                                                                   distance=Distance.COSINE))  # distance距离计算方式
+        if not res:
+            raise Exception("创建collection失败")
+    store = QdrantVectorStore(client=client, collection_name=collection_name, embedding=eb)
+    # 防止重复插入
+    s_doc_new = []
+    s_doc_ids = []
+    for doc in docs:
+        doc.id = md5(doc.page_content)
+        s_doc_new.append(doc)
+        s_doc_ids.append(doc.id)
+
+    added_ids = store.add_documents(s_doc_new, ids=s_doc_ids)
+    print(added_ids)
+
 
 if __name__ == "__main__":
-    test()
+    # 第一次运行时候需要运行chroma_store()，将文本存储到chroma中, 后续可以直接运行chroma_search()
+    chroma_store()
+    chroma_search()
